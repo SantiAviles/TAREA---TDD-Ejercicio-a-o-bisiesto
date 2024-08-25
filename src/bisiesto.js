c83eaef9988c2b1f712d6bba55c7b0c94e757d97
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-08-25 17:44:56 -0400

    Segndo commit, añadiendo la segunda condicion, Todos los años divisibles por 100 pero no por 400 NO son años bisiestos

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,7 +1,9 @@
 function verificarBisiesto(numero) {
     if(numero % 400 === 0) {
         return "Es Bisiesto"
-    };
+    } else if(numero % 100 === 0) {
+        return "No es Bisiesto"
+    };  
 };
 
 export default verificarBisiesto;
--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -4,4 +4,7 @@ describe("Bisiesto", () => {
     it("Todos los años divisibles por 400 SON años bisiestos", () => {
         expect(verificarBisiesto(2000)).toEqual("Es Bisiesto");
     });
+    it("Todos los años divisibles por 100 pero no por 400 NO son años bisiestos", () => {
+        expect(verificarBisiesto(1700)).toEqual("No es Bisiesto");
+    });
 });
